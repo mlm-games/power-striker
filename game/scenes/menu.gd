ff0688085441addfd1b0b46f4d72d53f586deab5
mlm-games <125530737+mlm-games@users.noreply.github.com>
2025-07-20 17:47:44 +0530

    add credits to game

--- game/scenes/menu.gd
+++ game/scenes/menu.gd
@@ -5,3 +5,4 @@ func _ready() -> void:
 	%PlayButton.pressed.connect(STransitions.change_scene_with_transition.bind("uid://bqkcvdot8ygo3"))
 	#%SettingsButton.pressed.connect(add_child.bind(preload("uid://dp42fom7cc3n0").instantiate()))
 	%QuitButton.pressed.connect(get_tree().quit)
+	%CreditsButton.pressed.connect(STransitions.change_scene_with_transition.bind("uid://bq0gelfcjnqvg"))
